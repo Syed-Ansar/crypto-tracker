5a55189d04813d03309cc85d34da3b69c46e87c3
Syed Ansar <syedansar6666@gmail.com>
2023-07-31 17:44:26 +0530

    added links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,35 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
-		<div className='footer'>
-			<Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-				{/*Crypto Galaxy &copy; 2022 | All Rights Reserved*/}
-				Made By <a style={{color:'white'}} href="https://www.aryankumar.in">Syed Ansar</a> | Github: <a style={{color:'white'}} href="https://github.com/ArynKr/crypto-galaxy"> Crypto Galaxy </a>
-			</Typography.Title>
-			<Space>
-				<Link to='/'>Home</Link>
-				<Link to='/exchanges'>Exchanges</Link>
-				<Link to='/news'>News</Link>
-			</Space>
-		</div>
-	);
+    <div className="footer">
+      <Typography.Title
+        level={5}
+        style={{ color: "white", textAlign: "center" }}
+      >
+        {/*Crypto Galaxy &copy; 2022 | All Rights Reserved*/}
+        Made By{" "}
+        <a
+          style={{ color: "white" }}
+          href="https://www.linkedin.com/in/syed--ansar/"
+        >
+          Syed Ansar
+        </a>{" "}
+        | Github:{" "}
+        <a
+          style={{ color: "white" }}
+          href="https://github.com/Syed-Ansar/crypto-tracker"
+        >
+          {" "}
+          Crypto Tracker App{" "}
+        </a>
+      </Typography.Title>
+      <Space>
+        <Link to="/">Home</Link>
+        <Link to="/exchanges">Exchanges</Link>
+        <Link to="/news">News</Link>
+      </Space>
+    </div>
+  );
 };
 
 export default Footer;
